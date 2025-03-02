4ab06785d81d63c09f266bc2ef138959f40ec8cc
Dimitri Sukhankin <berna@detta.dev>
2025-03-02 17:43:24 +0400

    fixed issue where after you empty row header, you can't write there anymore

--- src/components/NewRowHeader.ts
+++ src/components/NewRowHeader.ts
@@ -1,3 +1,3 @@
-const NEW_ROW_HEADER = `<span contenteditable>ICA Training Facility</span>`;
+const NEW_ROW_HEADER = `<p contenteditable>ICA Training Facility</p>`;
 
 export default NEW_ROW_HEADER;
