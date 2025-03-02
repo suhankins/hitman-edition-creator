22ab37d80b610556ded903f450548e7cfaf8e9f3
Dimitri Sukhankin <berna@detta.dev>
2025-03-03 01:53:54 +0400

    added default state

---
+++ src/components/Table/DefaultTableState.ts
@@ -0,0 +1,152 @@
+const DEFAULT_STATE = {
+    editions: [
+        {
+            name: 'Free Starter Pack',
+            image: 'todo',
+        },
+        {
+            name: 'Episode: Sapienza',
+            image: 'todo',
+        },
+        {
+            name: 'Part One',
+            image: 'todo',
+        },
+        {
+            name: 'Standard Edition',
+            image: 'todo',
+        },
+        {
+            name: 'Deluxe Edition',
+            image: 'todo',
+        },
+    ],
+    content: [
+        {
+            name: 'Free Seasonal Content',
+            ownership: [true, true, true, true, true],
+        },
+        {
+            name: 'ICA Training Missions',
+            ownership: [true, true, true, true, true],
+        },
+        {
+            name: 'Paris',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: 'Sapienza',
+            ownership: [false, true, true, true, true],
+        },
+        {
+            name: 'Marrakesh',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: 'Bangkok',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: 'Colorado',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: 'Hokkaido',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: 'Patient Zero Campaign',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: '3 Deluxe Escalations',
+            ownership: [false, false, true, true, true],
+        },
+        {
+            name: 'Freelancer Game Mode',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'VR Mode',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: "Hawke's Bay",
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Miami',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Santa Fortuna',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Mumbai',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Whittleton Creek',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Isle of Sgàil',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Dubai',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Dartmoor',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Berlin',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Chongqing',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Mendoza',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Carpathian Mountains',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'Ambrose Island',
+            ownership: [false, false, false, true, true],
+        },
+        {
+            name: 'New York',
+            ownership: [false, false, false, false, true],
+        },
+        {
+            name: 'Haven Island',
+            ownership: [false, false, false, false, true],
+        },
+        {
+            name: '7 Deadly Sins',
+            ownership: [false, false, false, false, true],
+        },
+        {
+            name: '2 Specal Assigment Packs',
+            ownership: [false, false, false, false, true],
+        },
+        {
+            name: '6 Deluxe Escalations',
+            ownership: [false, false, false, false, true],
+        },
+        {
+            name: '4 packs of suits and items',
+            ownership: [false, false, false, false, true],
+        },
+    ],
+};
+
+export default DEFAULT_STATE;
